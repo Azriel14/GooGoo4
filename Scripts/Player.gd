2998fa7b33ccc89a9d942cf9d943a2ec904aae02
Weebus <awesomezoroark@gmail.com>
2023-05-07 23:48:49 +1000

    Added player character

---
+++ Scripts/Player.gd
@@ -0,0 +1,65 @@
+extends KinematicBody2D
+
+export var speed = 200
+var momentum = Vector2.ZERO
+var screenSize = Vector2.ZERO
+var lastDirection = Vector2.ZERO
+var isMoving = false
+onready var animation = $Animation
+
+func _ready():
+	screenSize = get_viewport_rect().size
+
+func _physics_process(delta):
+	# Movement
+	var input_vector = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	if input_vector == Vector2.ZERO:
+		momentum = Vector2.ZERO
+	else:
+		momentum = momentum.move_toward(input_vector * speed, speed * delta)
+	move_and_slide(momentum)
+
+	# Screen Wrap
+	if position.x < 0:
+		position.x = screenSize.x
+	elif position.x > screenSize.x:
+		position.x = 0
+	if position.y < 0:
+		position.y = screenSize.y
+	elif position.y > screenSize.y:
+		position.y = 0
+
+	# Animation
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		direction = Vector2.UP
+	elif Input.is_action_pressed("ui_down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_pressed("ui_right"):
+		direction = Vector2.RIGHT
+		$Silvangoisse.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		direction = Vector2.LEFT
+		$Silvangoisse.flip_h = true
+
+	if direction != Vector2.ZERO:
+		lastDirection = direction
+		isMoving = true
+		if direction == Vector2.UP:
+			animation.play("WalkU")
+		elif direction == Vector2.DOWN:
+			animation.play("WalkD")
+		else:
+			animation.play("WalkLR")
+	else:
+		isMoving = false
+		if lastDirection == Vector2.UP:
+			animation.play("IdleU")
+		elif lastDirection == Vector2.DOWN:
+			animation.play("IdleD")
+		else:
+			animation.play("IdleLR")
+			$Silvangoisse.flip_h = lastDirection == Vector2.LEFT
