9367261a97e3eb5d6e5ea6e0faa73a6716971f6d
SonicMam <dapar8@gmail.com>
2023-05-12 19:12:41 +1000

    made inventory be able to close after opening it

--- Scripts/Inventory.gd
+++ Scripts/Inventory.gd
@@ -15,7 +15,7 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("ui_inventory"):
 		toggle_visibility()
-		set_process_input(false)
+		#set_process_input(false)
 		set_process_unhandled_input(false)
 
 func toggle_visibility():
@@ -26,7 +26,7 @@ func toggle_visibility():
 
 func hide_inventory():
 	is_visible = false
-	tween.interpolate_property(self, "rect_position:y", rect_position.y, get_viewport_rect().size.y, ANIMATION_DURATION, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	tween.interpolate_property(self, "rect_position:y", rect_position.y, get_viewport_rect().size.y + INVENTORY_HEIGHT, ANIMATION_DURATION, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	tween.start()
 
 func show_inventory():
